636f9082d227b6d2f0c17f09f4143c37981487a9
edmilson-da-conceicao <edmilson.conceicao09@gmail.com>
2021-04-22 04:49:10 +0200

    Added Data Structure(JS): Hash Table

---
+++ Data Structures/Hash Table/implementHashTable.js
@@ -0,0 +1,38 @@
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  }
+
+  set(key, value) {
+    let address = this._hash(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+    this.data[address].push([key, value]);
+    return this.data;
+  }
+
+  get(key) {
+    let address = this._hash(key);
+    const currentBacket = this.data[address];
+    if (currentBacket) {
+      for (let i = 0; i < currentBacket.length; i++) {
+        if (currentBacket[i][0] === key) {
+          return currentBacket[i][1];
+        } else {
+          return -1;
+        }
+      }
+    } else {
+      return -1;
+    }
+  }
+}
