2a04bea7052c685c4bcc6141f7344816c5535297
edmilson-da-conceicao <edmilson.conceicao09@gmail.com>
2021-04-27 22:35:40 +0200

    Add Algorithm: Fibonacci Recursion

---
+++ Algorithms/Recursion/fibonacci.js
@@ -0,0 +1,16 @@
+function fibonacciRecursive(n) {
+  if(n < 2) {
+    return n;
+  }
+  return fibonacciRecursive(n-1) + fibonacciRecursive(n-2);
+}
+
+function fibonacciIterative(n) {
+  let arr = [0, 1, 1]
+
+  for(let i = 3; i <= n; i++) {
+    arr.push(arr[i-1] + arr[i-2])
+  }
+
+  return arr[n];
+}
