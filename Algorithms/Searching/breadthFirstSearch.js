ca32d3722ca8166f69352d308d0c7da943b8ed6c
edmilson-da-conceicao <edmilson.conceicao09@gmail.com>
2021-04-28 18:01:03 +0200

    Add Algorithm: Breadth First Search

---
+++ Algorithms/Searching/breadthFirstSearch.js
@@ -0,0 +1,44 @@
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+function bfs() {
+  let currentNode = this.root;
+  let queue = [];
+  let list = [];
+
+  queue.push(currentNode);
+
+  while (queue.length > 0) {
+    currentNode = queue.shift();
+    list.push(currentNode.value);
+    if (currentNode.left) {
+      queue.push(currentNode.left);
+    }
+    if (currentNode.right) {
+      queue.push(currentNode.right);
+    }
+  }
+  return list;
+}
+
+function bfsRecursion(queue, list) {
+  if (!queue.length) {
+    return list;
+  }
+  const currentNode = queue.shift();
+  list.push(currentNode.value);
+
+  if (currentNode.left) {
+    queue.push(currentNode.left);
+  }
+  if (currentNode.right) {
+    queue.push(currentNode.right);
+  }
+
+  return this.bfsRecursion(queue, list);
+}
