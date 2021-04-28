1de54239543d6f85c773058e796f6438c21613d8
edmilson-da-conceicao <edmilson.conceicao09@gmail.com>
2021-04-28 13:00:45 +0200

    Refactor lookup() in Binary Search Tree

--- Data Structures/Trees/BST.js
+++ Data Structures/Trees/BST.js
@@ -42,34 +42,22 @@ class BinarySearchTree {
 
   lookup(value) {
     let node = new Node(value);
-    let cond = true
     if (this.root == null) {
       console.log("Is Empty");
     } else if (this.root.value === node.value) {
       return this.root;
     } else {
       let tempNode = this.root;
-      while (cond) {
+      while (tempNode) { // == tempNode != null
         if (node.value > tempNode.value) {
           tempNode = tempNode.right;
-          if (tempNode == null) {
-            console.log(`Value ${value} not Found`)
-            cond = false;
-          } else if (tempNode.value === node.value) {
-            cond = false;
-            return tempNode;
-          }
         } else if (node.value < tempNode.value) {
           tempNode = tempNode.left;
-          if (tempNode == null) {
-            console.log(`Value ${value} not Found`)
-            cond = false;
-          } else if (tempNode.value === node.value) {
-            cond = false;
-            return tempNode;
-          }
+        } else if (tempNode.value === node.value) {
+          return tempNode;
         }
       }
+      return null; // Error 404 - Node Not Found
     }
   }
 
