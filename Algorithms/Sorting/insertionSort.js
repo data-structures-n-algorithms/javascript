4397e88ece6643fdeaa49989f5889737cf085f13
edmilson-da-conceicao <edmilson.conceicao09@gmail.com>
2021-05-02 14:55:41 +0200

    Add Algorithm: Insertion Sort

---
+++ Algorithms/Sorting/insertionSort.js
@@ -0,0 +1,17 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function insertionSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < array[0]) {
+      array.unshift(array.splice(i, 1)[0]);
+    } else {
+      if (array[i] < array[i - 1]) {
+        for (var j = 1; j < i; j++) {
+          if (array[i] >= array[j - 1] && array[i] < array[j]) {
+            array.splice(j, 0, array.splice(i, 1)[0]);
+          }
+        }
+      }
+    }
+  }
+}
